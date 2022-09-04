c987d56dd2706dcc0c6f2944b2034217b4449411
NathanaelGazzard <nathanaelgazzard@gmail.com>
2022-09-04 14:49:58 +1000

    Added Minimap
    Added a minimap. Further details such as how it is displayed can be refined in future. Also, for now, it is just using the google maps reference image - we may need to edit this to remove landmarks etc.

---
+++ Scripts/MiniMapScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapScript : MonoBehaviour
+{
+    [SerializeField] Vector3 offsetValue;
+    [SerializeField] Vector3 mainMapPos;
+
+    [SerializeField] Transform playerCamRootRef;
+
+    [SerializeField] Transform cameraRef;
+
+    float camHeight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = mainMapPos + offsetValue; // sets the duplicate map to its offset location
+        camHeight = cameraRef.position.y;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newCamPos = playerCamRootRef.position + offsetValue;
+        newCamPos.y = camHeight;
+        cameraRef.position = newCamPos;
+        cameraRef.eulerAngles = new Vector3(90, playerCamRootRef.eulerAngles.y, 0);
+    }
+}
